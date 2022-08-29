36bfb51c51af6e7e122f2d5feff731542a9f158e
Sergio De Luca <sdlbsso@gmail.com>
2022-08-29 14:45:38 +0000

    servidor de node

--- 2020/node/server.js
+++ 2020/node/server.js
@@ -4,7 +4,7 @@ const host = 'localhost';
 const port = 5000;
 
 // config.vm.network "private_network", ip: "55.55.55.5" #en archivo de vagrant
-
+//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
 const requestListener = function (req, res) {
     res.writeHead(200);
     res.end("Servidor con node de prueba!");
