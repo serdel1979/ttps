d61dc5cf0ad1f7d4449dd146c73470645ee6795f
Sergio De Luca <sdlbsso@gmail.com>
2022-08-29 14:40:30 +0000

    servidor de node para prueba con vagrant

---
+++ 2020/node/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 5000;
+
+// config.vm.network "private_network", ip: "55.55.55.5" #en archivo de vagrant
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Servidor con node de prueba!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
