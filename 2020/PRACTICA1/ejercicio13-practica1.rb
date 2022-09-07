bf583c90e94d713b5862180affe67ac54452d6b6
Sergio De Luca <sdlbsso@gmail.com>
2022-09-07 12:24:26 +0000

    ejercicio 13 with_index

--- 2020/PRACTICA1/ejercicio13-practica1.rb
+++ 2020/PRACTICA1/ejercicio13-practica1.rb
@@ -1,5 +1,7 @@
-def listar(dic, pegamento=": ")
-    dic.each do |key, value|
-    puts "#{key}#{pegamento}#{value}\n"
-    end
+def listar(hash, pegamento=": ")
+    str=""
+    hash.each.with_index{|val, index| str << "#{index+1}. #{val[0]}#{pegamento}#{val[1]}\n"}
+    str
 end
+
+puts listar({perros: 2, gatos: 2, peces: 0, aves:0})
