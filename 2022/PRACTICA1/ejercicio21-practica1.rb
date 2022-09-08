d72f76197b393236aae3900f6f941879fddff27b
Sergio De Luca <sdlbsso@gmail.com>
2022-09-08 21:37:39 +0000

    haciendo el 21

--- 2022/PRACTICA1/ejercicio21-practica1.rb
+++ 2022/PRACTICA1/ejercicio21-practica1.rb
@@ -1,11 +1,5 @@
 def multiplos_de_3_y_5(valor=100)
-    tot = 0
-    valor.times do |time|
-        if time % 3 ==0 && time % 5 == 0 
-            tot = tot + time
-        end
-    end
-    tot
+   (1..valor).map {|i| (i % 3 ==0 && i % 5 == 0) ? i }
 end
 
 puts multiplos_de_3_y_5(100)
