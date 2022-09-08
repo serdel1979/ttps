bb0c2431977e155dd21cc554981626461336f149
Sergio De Luca <sdlbsso@gmail.com>
2022-09-08 14:20:23 +0000

    ejercicio 22de pr1 incompleto

--- 2022/PRACTICA1/ejercicio22-practica1.rb
+++ 2022/PRACTICA1/ejercicio22-practica1.rb
@@ -1,2 +1,14 @@
-def multiplos_de([3, 5], 100)
+def multiplos_de(numeros, valor=100)
+    arr = []
+    valor.times do |time|
+        numeros.each { |elemento| 
+            if time % elemento == 0
+                puts "#{elemento} es multiplo de #{time}"
+                arr << elemento
+            end
+        }
+    end
+    arr
 end
+
+puts multiplos_de([3,5],100)
