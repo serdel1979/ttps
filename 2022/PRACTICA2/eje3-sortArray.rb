eb2076a33a72fb91f2dae34197538efa9853fb76
Sergio De Luca <sdlbsso@gmail.com>
2022-09-16 01:20:05 +0000

    ejercicio 2 y 3

--- 2022/PRACTICA2/eje3-sortArray.rb
+++ 2022/PRACTICA2/eje3-sortArray.rb
@@ -1,7 +1,7 @@
 def ordenar(*datos)
-    p datos[0]
-    datos.entries.sort
+    p datos
+    datos.sort
 end
 
 entrada = [10,20,5,6,7,1,9,8]
-p ordenar(entrada=entrada.map.entries)
+p ordenar(*entrada)
